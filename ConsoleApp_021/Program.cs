7bd785d17cd47300de5a02fb5e30365fde0cb6f7
Samir Bagamaev <05ru4315@gmail.com>
2022-12-23 21:36:17 +0300

    ЗАДАЧА 21

---
+++ ConsoleApp_021/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2 формула на память
+
+Console.WriteLine("Введите координаты точки X ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Y ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Z ");
+int Z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки X1 ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки Y1 ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки Z1 ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(X1 - X, 2) + Math.Pow(Y1 - Y, 2) + Math.Pow(Z1 - Z, 2));
+
+Console.WriteLine($"Расстояние между точками = {Math.Round(result)}");
