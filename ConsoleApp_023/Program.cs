28bf23ca5b644ad807a1d13b5a35136947d5114f
Samir Bagamaev <05ru4315@gmail.com>
2022-12-23 21:43:42 +0300

    ЗАДАЧА 23

---
+++ ConsoleApp_023/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и
+// выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(i * i * i);
+}
